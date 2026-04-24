532c909806a85e19f1c15c5dc84145c44f7d04a0
nisha-goswami11 <nishagoswami1102@gmail.com>
2026-04-24 11:15:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    document.body.classList.toggle('dark');
+}
+
+//   Simple scroll animation
+const cards = document.querySelectorAll('.card');
+window.addEventListener('scroll', () => {
+    cards.forEach(card => {
+        const top = card.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            card.style.opacity = 1;
+            card.style.transform = 'translateY(0)';
+        }
+    })
+})
